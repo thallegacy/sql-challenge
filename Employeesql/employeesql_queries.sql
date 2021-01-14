33ed7cad398875ee7bb8206e2c9b8b654c4032e6
thallegacy <thallegacy@users.noreply.github.com>
2021-01-14 18:51:40 -0500

    second query added

--- Employeesql/employeesql_queries.sql
+++ Employeesql/employeesql_queries.sql
@@ -3,3 +3,8 @@ SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
 LEFT JOIN salaries s ON
 e.emp_no = s.emp_no;
+
+-- List first name, last name, and hire date for employees who were hired in 1986
+SELECT first_name, last_name, hire_date from employees
+WHERE hire_date >= '1986-01-01'
+AND hire_date <= '1986-12-31';
