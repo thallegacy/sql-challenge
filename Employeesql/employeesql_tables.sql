6b9242c5a6b4d2b738fe0bb3010c9ab7811a15ec
thallegacy <thallegacy@users.noreply.github.com>
2021-01-13 21:41:10 -0500

    create dept employee,manager and salary tables

--- Employeesql/employeesql_tables.sql
+++ Employeesql/employeesql_tables.sql
@@ -9,8 +9,8 @@ DROP TABLE IF EXISTS salaries;
 
 -- create titles table
 CREATE TABLE titles (
-	title_id VARCHAR(20) PRIMARY KEY NOT NULL,
-    title VARCHAR (50)  NOT NULL
+	title_id VARCHAR (20) PRIMARY KEY NOT NULL,
+    title VARCHAR (50) NOT NULL
 );
 
 -- create departments table
@@ -22,11 +22,34 @@ CREATE TABLE departments (
 -- create employees table
 CREATE TABLE Employees (
     emp_no INT  PRIMARY KEY NOT NULL,
-    emp_title_id VARCHAR(20) NOT NULL,
+    emp_title_id VARCHAR (20) NOT NULL,
 	birth_date DATE  NOT NULL,
-    first_name VARCHAR  (30) NOT NULL,
-    last_name VARCHAR  (30) NOT NULL,
-    sex VARCHAR (5)  NOT NULL,
-    hire_date DATE   NOT NULL,
-	FOREIGN KEY (emp_title_id) REFERENCES departments(emp_title_id)
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    sex VARCHAR (5) NOT NULL,
+    hire_date DATE  NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- create department_employees table
+CREATE TABLE department_employees (
+    emp_no INT NOT NULL,
+	dept_no VARCHAR (20) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+-- create department_managers table
+CREATE TABLE department_managers (
+    dept_no VARCHAR  NOT NULL,
+    emp_no int  NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- create salaries table
+CREATE TABLE salaries (
+    emp_no int  NOT NULL,
+	salary INTEGER NOT NULL,  
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
