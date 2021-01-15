f07929b9ab6b694a3e890b9c5a2ee984adf005b4
thallegacy <thallegacy@users.noreply.github.com>
2021-01-14 22:52:47 -0500

    sixth query added

--- Employeesql/employeesql_queries.sql
+++ Employeesql/employeesql_queries.sql
@@ -28,8 +28,18 @@ de.dept_no = d.dept_no
 LEFT JOIN employees e ON
 de.emp_no = e.emp_no;
 
+
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, last_name, sex 
 FROM employees
 WHERE first_name = 'Hercules'
 AND last_name LIKE 'B%';
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM department_employees de
+LEFT JOIN departments d ON
+de.dept_no = d.dept_no
+LEFT JOIN employees e ON
+de.emp_no = e.emp_no
+WHERE d.dept_name = 'Sales';
