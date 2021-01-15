2a41764d8271b60e469638b985837f6c70a99e92
thallegacy <thallegacy@users.noreply.github.com>
2021-01-14 22:26:12 -0500

    fourth query added

--- Employeesql/employeesql_queries.sql
+++ Employeesql/employeesql_queries.sql
@@ -9,11 +9,20 @@ SELECT first_name, last_name, hire_date from employees
 WHERE hire_date >= '1986-01-01'
 AND hire_date <= '1986-12-31';
 
---List the manager of each department with the following information: 
---department number, department name, the manager's employee number, last name, first name.
+-- List the manager of each department with the following information: 
+-- department number, department name, the manager's employee number, last name, first name.
 SELECT dm.dept_no, d.dept_name, dm.emp_no, e.first_name, e.last_name
 FROM department_managers dm
 LEFT JOIN departments d ON
 dm.dept_no = d.dept_no
 LEFT JOIN employees e ON
 dm.emp_no = e.emp_no;
+
+-- List the department of each employee with the following information:
+-- employee number, last name, first name, and department name.
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM department_employees de
+LEFT JOIN departments d ON
+de.dept_no = d.dept_no
+LEFT JOIN employees e ON
+de.emp_no = e.emp_no;
